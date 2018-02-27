f5d200a02ba00ddbd22cfea236baee938cf305fb
redsudo <email@email.com>
2018-02-27 01:49:31 -0800

    Added settings descriptions to settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,13 +3,29 @@ Store your redsea download presets here
 
 You may modify/add/remove as you wish. The only preset which must exist is "default"
 and you may change the default as needed.
+
+=== Options ===
+keep_cover_jpg: save the cover art to the album directory
+embed_album_art: embed the album art into each audio file
+save_album_json: save the album metadata as a json file
+tries: number of attempts before giving up on the download
+path: where to store downloaded files
+track_format: naming mask for tracks
+album_format: naming mask for abum directories
+
+=== Formats ===
+HI_RES: MQA Format / 24bit FLAC with high-frequency "folded" data
+LOSSLESS: 16bit FLAC
+HIGH: 320Kbps AAC
+LOW: 96Kbps AAC
+
 '''
 
 from accounts import ACCOUNTS
 
 PRESETS = {
 
-    # Default settings only download LOSSLESS
+    # Default settings / only download LOSSLESS
     "default": {
         "keep_cover_jpg": True,
         "embed_album_art": True,
@@ -39,8 +55,8 @@ PRESETS = {
         "LOW": True
     },
 
-    # This preset will allow only LOSSLESS and MQA files
-    "mqa_enabled": {
+    # This preset will allow LOSSLESS and MQA files only
+    "mqa_lossless": {
         "keep_cover_jpg": True,
         "embed_album_art": True,
         "save_album_json": False,
